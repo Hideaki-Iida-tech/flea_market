7fcca745754ec8631c694aefc2e853247e61000e
iida hideaki <hideaki.19790727@gmail.com>
2025-10-09 21:19:29 +0900

    coachtech初級模擬案件44日目ver1.0（メール認証機能実装）

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -7,17 +7,24 @@
 @endsection
 @section('content')
 <div class="verify-email-form">
-    <form action="" class="verify-email-form-inner" method="post">
+    <div class="verify-email-form-inner">
         <div class="verify-email-message">
             登録いただいたメールアドレスに認証メールを送付しました。<br />
             メール認証を完了してください。
         </div>
         <div class="verify-email-exec">
-            <button class="verify-email-exec-button">認証はこちらから</button>
+            <form action="{{ url('http://localhost:8025') }}" method="get">
+                <button class="verify-email-exec-button">認証はこちらから</button>
+            </form>
         </div>
+
+
         <div class="verify-email-resend">
-            <button class="verify-email-resend-button">認証メールを再送する</button>
+            <form action="{{ route('verification.send') }}" method="get">
+                <button class="verify-email-resend-button">認証メールを再送する</button>
+            </form>
         </div>
-    </form>
+
+    </div>
 </div>
 @endsection
